692dc9ccf275f0c32ed29dca10e809d898d2b2e9
chrisHembree <111545379+chrisHembree@users.noreply.github.com>
2022-10-14 17:08:44 -0500

    2nd commit

---
+++ src/app/image-creator/image-creator.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-image-creator',
+  templateUrl: './image-creator.component.html',
+  styleUrls: ['./image-creator.component.css']
+})
+export class ImageCreatorComponent implements OnInit {
+
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
+
+
+
