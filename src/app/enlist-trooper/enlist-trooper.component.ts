5707b164d421121d397dcca658dd9dc7639b7fdc
chrisHembree <chembree08@gmail.com>
2022-11-12 21:55:24 -0600

    Co-authored-by: kylesteinmann <kylesteinmann@users.noreply.github.com> Co-authored-by: Heath Falkenrath <theGeekkit@users.noreply.github.com>

--- src/app/enlist-trooper/enlist-trooper.component.ts
+++ src/app/enlist-trooper/enlist-trooper.component.ts
@@ -1,20 +1,40 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms'
+import { NgForm } from '@angular/forms';
 import { TrooperService } from '../trooper.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-enlist-trooper',
   templateUrl: './enlist-trooper.component.html',
-  styleUrls: ['./enlist-trooper.component.css']
+  styleUrls: ['./enlist-trooper.component.css'],
 })
 export class EnlistTrooperComponent implements OnInit {
+  constructor(
+    private trooperService: TrooperService,
+    private http: HttpClient
+  ) {}
 
-  constructor(private trooperService: TrooperService) { }
-
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
   onEnlist(form: NgForm) {
-    console.log(form.value)
-   this.trooperService.troopers.push(form.value)
+    console.log(form.value);
+    const trooperName = form.value.name;
+    const trooperSkillsets = form.value.skillsets;
+    const trooperUrl = form.value.imagePath;
+
+    const postTrooper = {
+      name: trooperName,
+      skillsets: trooperSkillsets,
+      imagepath: trooperUrl,
+    };
+
+    this.http
+      .post(
+        'https://trooer-8e20e-default-rtdb.firebaseio.com/enlisted-troopers',
+        postTrooper
+      )
+
+      .subscribe();
+
+    this.trooperService.troopers.push(form.value);
   }
 }
